c8791c68c952c9d16255a3f9b1ee713d8c0a83dc
Efe Beydogan <efebeydogan@hotmail.com>
2024-02-12 16:30:35 +0100

    added mongodb

--- Lab1/swagger_server/service/student_service.py
+++ Lab1/swagger_server/service/student_service.py
@@ -1,41 +1,36 @@
 import os
 import tempfile
 from functools import reduce
+from pymongo import MongoClient
 
-from tinydb import TinyDB, Query
-
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
-
+MONGO_URI = os.environ['MONGO_URI']
+db = MongoClient(MONGO_URI)
+database_name = db['tutorial1']
+collection_name = database_name['student_db']
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
+    res = collection_name.find_one({ 'first_name': student.first_name, 'last_name': student.last_name })
+    if not res:
         return 'already exists', 409
-
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
+    doc_id = collection_name.insert_one(student.to_dict())
     return student.student_id
 
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = collection_name.find_one({'student_id': student_id})
     if not student:
         return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
-    return student
+    return {
+        'first_name': student['first_name'],
+        'gradeRecords': student['grade_records'],
+        'last_name': student['last_name'],
+        'student_id': student['student_id']
+    }
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = collection_name.find({'student_id': student_id})
     if not student:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
+    collection_name.delete_many({'student_id': student_id})
     return student_id
